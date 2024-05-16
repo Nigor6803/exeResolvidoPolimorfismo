9742d74255567345d7c11246c8cd20c5d1b30593
Igor Nascimento <nigor6803@gmail.com>
2024-05-16 13:06:51 -0300

    quase finalizado

--- src/aplicacao/Progama.java
+++ src/aplicacao/Progama.java
@@ -1,9 +1,31 @@
 package aplicacao;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entidade.Empregado;
+
 public class Progama {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner edd = new Scanner(System.in);
+		
+		List <Empregado> lista = new ArrayList<>();
+		
+		System.out.print("QUANTIDADE DE EMPREGADOS:");
+		int n = edd.nextInt();
+		
+		for(int i = 1;i<=n;i++) {
+			System.out.println("EMPREGADO #"+i+":");
+			System.out.print("TERCERIZADO:(S/N)");
+			char ch = edd.next().charAt(0);
+			
+		}
+		
+		edd.close();
 
 	}
 
